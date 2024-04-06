64da047c24b589b51441fbc06610fc909b72215c
Mustafa-Ashfaq81 <mustafaaplus52@gmail.com>
2024-04-06 22:06:02 +0500

    add cnn

--- DehazeFormer/models/dehazeformer.py
+++ DehazeFormer/models/dehazeformer.py
@@ -37,18 +37,18 @@ class CustomizedCNNFeatureExtractor(nn.Module):
 class CustomCNNFeatureExtractor(nn.Module):
     def __init__(self):
         super(CustomCNNFeatureExtractor, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
         self.relu = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
 
-        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1) # Downsample here
-        self.bn3 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, stride=2, padding=1) # Downsample here
+        self.bn3 = nn.BatchNorm2d(32)
 
-        self.conv4 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(32, 8, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(8)
 
     def forward(self, x):
         x = self.relu(self.bn1(self.conv1(x)))
@@ -439,8 +439,8 @@ class DehazeFormer(nn.Module):
 		super(DehazeFormer, self).__init__()
 
 		# Initialize the CNN feature extractor
-		self.cnn_extractor = CustomizedCNNFeatureExtractor() #CustomCNNFeatureExtractor()#CNNFeatureExtractor(pretrained=True)
-		self.channel_adjustment_layer = nn.Conv2d(in_channels=256, out_channels=3, kernel_size=1)
+		self.cnn_extractor = CustomCNNFeatureExtractor() #CustomizedCNNFeatureExtractor() #CNNFeatureExtractor(pretrained=True)
+		self.channel_adjustment_layer = nn.Conv2d(in_channels=8, out_channels=3, kernel_size=1)
 
 		# setting
 		self.patch_size = 4
@@ -449,7 +449,7 @@ class DehazeFormer(nn.Module):
 
 		# split image into non-overlapping patches
 		self.patch_embed = PatchEmbed(
-			patch_size=1, in_chans=256, embed_dim=embed_dims[0], kernel_size=3)
+			patch_size=1, in_chans=8, embed_dim=embed_dims[0], kernel_size=3)
 
 		# backbone
 		self.layer1 = BasicLayer(network_depth=sum(depths), dim=embed_dims[0], depth=depths[0],
@@ -639,9 +639,9 @@ def dehazeformer_l():
 		attn_ratio=[1/4, 1/2, 3/4, 0, 0],
 		conv_type=['Conv', 'Conv', 'Conv', 'Conv', 'Conv'])
 
-# if __name__ == '__main__':
-# 	model = dehazeformer_t()
-# 	shape = (8, 3, 64, 64)
-# 	img = torch.randn(*shape)
-# 	output = model(img)
-# 	print(output.shape)
+if __name__ == '__main__':
+	model = dehazeformer_t()
+	shape = (8, 3, 64, 64)
+	img = torch.randn(*shape)
+	output = model(img)
+	print(output.shape)
